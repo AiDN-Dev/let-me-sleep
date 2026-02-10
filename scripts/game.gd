bd87ec6b57601103e12de23e9ae09d98f495d34b
AiDN-Dev <aidn.p1993@proton.me>
2026-02-10 12:13:14 +0000

    Initial playable prototype: Sleep QTE Loop

---
+++ scripts/game.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@onready var qte_window = $UI/QTEWindow
+
+var rng := RandomNumberGenerator.new()
+
+func _ready() -> void:
+	rng.randomize()
+	qte_window.connect("qte_finished", _on_qte_finished)
+	start_sleep()
+	
+func start_sleep() -> void:
+	await get_tree().create_timer(1.0).timeout
+	start_qte()
+	
+func start_qte():
+	var keys = [KEY_Z, KEY_X, KEY_C]
+	var key = keys.pick_random()
+	
+	var texts = [
+		"HOLD TO RELAX",
+		"TRY TO SLEEP",
+		"DO NOT WAKE UP",
+		"IGNORE THE THOUGHTS"
+	]
+	
+	qte_window.start_qte(texts.pick_random(), key)
+	
+func _on_qte_finished(success: bool) -> void:
+	if success:
+		start_sleep()
+	else:
+		# Right now failure restarts the loop
+		start_sleep()
